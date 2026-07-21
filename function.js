100e43660be6dd58970b431ba07323c154108a77
chapalida <cha.palida@gmail.com>
2026-07-21 13:14:22 +1000

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,10 @@
-window.addEventListener('click', function(event) {
+function hideOverlay() {
     var overlay = document.getElementById('overlay');
-    
+
     if (overlay && overlay.style.display !== 'none') {
         overlay.style.display = 'none';
     }
-});
+}
+
+window.addEventListener('click', hideOverlay);
+window.addEventListener('touchend', hideOverlay);
